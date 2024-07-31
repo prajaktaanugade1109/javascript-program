20fae808783002662677929750007ba1e253a461
prajaktaanugade1109 <prajaktaanugade123@gmail.com>
2024-07-31 15:21:38 +0530

    javascript variables
    javascript variables

---
+++ 01-variableAssign.js
@@ -0,0 +1,39 @@
+// Variable Declaration
+var myFullName;
+//Printing the value of myFullName variable
+console.log(myFullName);
+
+//Variable Initialization
+myFullName = "Prajakta Anugade";
+//Printing the value of myFullName variable
+console.log("My full Name is:", myFullName);
+
+//Updating the value of myFullName variable
+myFullName = "Prajakta Pandit Anugade";
+//Printing the updated value of myFullName variable
+console.log("My full Name is:", myFullName);
+
+//myLove variable dec &init
+var myLove = "I love only JS";
+console.log(myLove);
+
+var firstName = "Prajakta";
+console.log("My first name is:", firstName);
+
+var lastName = "Anugade";
+console.log("My last name is:", lastName);
+
+var age = 24;
+console.log("My age is:", age);
+
+var address = "Amanapur, Palus, Sangli";
+console.log("My permanent address is:", address);
+
+var pinCode = 416308;
+console.log("My pin code is:", pinCode);
+
+var gender = "Female";
+console.log("My gender is:", gender);
+
+var isMarried = "No";
+console.log("isMarried:", isMarried);
---
+++ 02-typeOf.js
@@ -0,0 +1,40 @@
+//Variable Declaration
+var bankName;
+console.log("My Bank name is:", bankName);
+var typeOfBankName = typeof bankName;
+console.log("The type of bank name is:", typeOfBankName);
+
+//Variable Initialization
+bankName = "SBI Bank";
+console.log("My Bank name is:", bankName);
+typeOfBankName = typeof bankName;
+console.log("The type of bank name is:", typeOfBankName);
+
+var marks = "90%";
+console.log("My graduation marks in percentage:", marks);
+var typeOfMarks = typeof marks;
+console.log("The datatype of marks variable is:", typeOfMarks);
+
+//Variable Declaration
+var isWorking;
+console.log("The working status:", isWorking);
+var typeOfIsWorking = typeof isWorking;
+console.log("The datatype of isWorking variable:", typeOfIsWorking);
+
+//Variable Initializations
+isWorking = true;
+console.log("The working status:", isWorking);
+typeOfIsWorking = typeof isWorking;
+console.log("The datatype of isWorking variable:", typeOfIsWorking);
+
+var totalCount = "Hundred and Seven";
+console.log("The total count is:", totalCount);
+var typeOfTotalCount = typeof totalCount;
+console.log("The datatype of totalCount variable is:", typeOfTotalCount);
+
+//Updated value
+totalCount = 107;
+console.log("The total count is:", totalCount);
+typeOfTotalCount = typeof totalCount;
+console.log("The datatype of totalCount variable is:", typeOfTotalCount);
+
