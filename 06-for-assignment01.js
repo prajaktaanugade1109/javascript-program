7400e918a448894fa967398af0eec981c82a368e
prajaktaanugade1109 <prajaktaanugade123@gmail.com>
2024-08-10 03:34:20 +0530

    for-loop-assignment
    for-loop-assignment

---
+++ 06-for-assignment01.js
@@ -0,0 +1,53 @@
+console.log('============= Print numbers from 5 to 15 by incrementing 1 ============');
+for (let index = 5; index <=15; index=index+1) {
+    console.log(index);
+       
+}
+
+
+console.log('============= Print numbers from 50 to 40 decrementing  by 1 ============');
+for (let index = 50; index>=40; index=index-1) {
+    console.log(index);
+       
+}
+
+
+console.log('============= Print first 15 odd numbers ============');
+for (let index =1; index <=30; index=index+2) {
+    console.log(index);  
+    
+}
+
+console.log('============= Print first 10 even numbers ============');
+for (let index = 0; index <=20; index=index+2) {
+    console.log(index);   
+    
+}
+
+
+console.log('============= Print table of 5 ============');
+for (let index = 5; index <=50; index=index+5) {
+    console.log(index);   
+    
+}
+
+
+console.log('============= Print table of 10 ============');
+for (let index = 10; index<=100; index=index+10) {
+    console.log(index);
+    
+    
+}
+    
+
+
+
+console.log('============= Print table of 10 reverse order ============');
+for (i= 100; i>=10; i=i-10) {
+    console.log(i);
+    
+    
+}
+
+
+
