442a181433a3da9a358df778387573ec82099596
prajaktaanugade1109 <prajaktaanugade123@gmail.com>
2024-08-16 17:17:18 +0530

    object-literals.js , 09-object-assig-B.js
    object-literals.js , 09-object-assig-B.js

---
+++ 09-object-assig-B.js
@@ -0,0 +1,49 @@
+
+console.log('====================== SBI Bank =======================');
+let sbiBank = {
+    bankName:"sbiBank",
+    location: "Sangli",
+    accountNo: 451100089505,
+    ifsc:"SBI08950390",
+    interestRate:8.5
+}
+console.log(sbiBank);
+
+console.log('====================== AXIS Bank =======================');
+let axisBank = {
+    bankName:"axisBank",
+    location: "Pune",
+    accountNo: 551100089505,
+    ifsc:"AXIS08950490",
+    interestRate: 9.2
+}
+console.log(axisBank);
+
+console.log('====================== HDFC Bank =======================');
+let hdfcBank = {
+    bankName:"hdfcBank",
+    location: "Kolhapur",
+    accountNo: 651100089505,
+    ifsc:"HDFC08950590",
+    interestRate:8.0
+}
+console.log(hdfcBank);
+
+console.log('====================== YES Bank =======================');
+let yesBank = {
+    bankName:"yesBank",
+    location: "Palus",
+    accountNo: 751100089505,
+    ifsc:"YES08950790",
+    interestRate:10.2
+}
+console.log(yesBank);
+
+
+
+
+
+
+
+
+
