40406349e348b9b2aaec0e4039d871750b2ca8ed
prajaktaanugade1109 <prajaktaanugade123@gmail.com>
2024-08-22 19:41:58 +0530

    10-hoisting.js,10-JSON.js,10-employee.json,11-session-storage.html
    10-hoisting.js,10-JSON.js,10-employee.json,11-session-storage.html

---
+++ 10-hoisting.js
@@ -0,0 +1,17 @@
+
+
+console.log(city);
+
+var city;
+
+greet ();
+function greet() {
+    console.log('Good Evening');
+       
+}
+
+//show (); //Not allowed because F.E never hosisted
+var show = function () {
+    console.log('Inside Function expression...');
+       
+}
